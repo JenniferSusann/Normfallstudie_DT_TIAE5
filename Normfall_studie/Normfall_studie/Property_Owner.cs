f91b19c9ddc6da1b3ea05a268978ea7b3d1d82c9
JenniferSusann <jennifermentner@hotmail.com>
2021-02-26 14:55:30 +0100

    First_Try
    just to start

---
+++ Normfall_studie/Normfall_studie/Property_Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Normfall_studie
+{
+    public class Property_Owner
+    {
+        public int PropertyOwnerId { get; set; }
+
+        public User Users { get; set; }
+    }
+}
---
+++ Normfall_studie/Normfall_studie/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Normfall_studie
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public ICollection<Property_Owner> Property_Owners { get; set; }
+    }
+}
